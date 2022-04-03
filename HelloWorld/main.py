076620ab7e6361ac3c076a85e4e3482cfef3f820
Kirill Feyzullin <47638256+JacKira@users.noreply.github.com>
2022-04-03 15:55:40 +0300

    Complete

--- HelloWorld/main.py
+++ HelloWorld/main.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello():
-    return 'Hello,\nWorld!'
+def index():
+    return render_template('index.html', text='Hello,\nWorld!')
 
 
 if __name__ == '__main__':
